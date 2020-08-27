c13c154f813f3f9b38ffed52e6b11bf42a6e51f2
DenzelHawking <denzelhawking@gmail.com>
2020-08-27 19:28:46 +0530

    switching tabs done

--- js/menu.js
+++ js/menu.js
@@ -2,13 +2,13 @@ let menuBtn = document.querySelector('.circle-menu');
 let menuItem = document.querySelectorAll('.circle-menu__item');
 
 
-menuBtn.onclick = function() {
-  menuBtn.classList.toggle('active');
-};
+menuBtn.onclick = () => menuBtn.classList.toggle('active');
 
-menuItem.forEach(elem => {
+menuItem.forEach((elem, index) => {
   elem.onclick = () => {
     event.stopPropagation();
     menuBtn.classList.remove('active');
+
+    getRotateSide(index)
   };
 });
--- js/rotate-tab.js
+++ js/rotate-tab.js
@@ -1,22 +1,22 @@
 let contentTab = document.querySelector('.content__tab-cube');
-
 let currentTab = document.querySelector('.current-tab');
 let nextTab = document.querySelector('.next-tab');
+let tabsInner = document.querySelectorAll('.pages');
 
 let rotateSide = ['top', 'bottom', 'left', 'right'];
+let currentTabIndex = 0;
+
+currentTab.innerHTML = tabsInner[0].innerHTML;
 
 
-function getRotateSide() {
+function getRotateSide(index) {
+  if (currentTabIndex == index) return;
+
   let randomNumber = Math.floor(Math.random() * 4);
+  currentTabIndex = index;
+  nextTab.innerHTML = tabsInner[index].innerHTML;
 
   contentTab.classList.add(`next-tab__${rotateSide[randomNumber]}`);
-  setTimeout(() => {
-    currentTab.innerHTML = nextTab.innerHTML;
-  }, 1500);
-
-  setTimeout(() => {
-    contentTab.classList.remove(`next-tab__${rotateSide[randomNumber]}`);
-  }, 2000);
+  setTimeout(() => currentTab.innerHTML = nextTab.innerHTML, 1500);
+  setTimeout(() => contentTab.classList.remove(`next-tab__${rotateSide[randomNumber]}`), 2000);
 };
-
-// document.body.onclick = getRotateSide;
