3bab8918c83139c0762378b902897623b09a3a47
DenzelHawking <denzelhawking@gmail.com>
2020-08-27 09:01:51 +0530

    rotate content done

---
+++ js/rotate-tab.js
@@ -0,0 +1,22 @@
+let contentTab = document.querySelector('.content__tab-cube');
+
+let currentTab = document.querySelector('.current-tab');
+let nextTab = document.querySelector('.next-tab');
+
+let rotateSide = ['top', 'bottom', 'left', 'right'];
+
+
+function getRotateSide() {
+  let randomNumber = Math.floor(Math.random() * 4);
+
+  contentTab.classList.add(`next-tab__${rotateSide[randomNumber]}`);
+  setTimeout(() => {
+    currentTab.innerHTML = nextTab.innerHTML;
+  }, 1500);
+
+  setTimeout(() => {
+    contentTab.classList.remove(`next-tab__${rotateSide[randomNumber]}`);
+  }, 2000);
+};
+
+document.body.onclick = getRotateSide;
