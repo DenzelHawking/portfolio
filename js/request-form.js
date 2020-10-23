cb438f2bd3b50d260c87bcb5f9b5154f3e93d74f
DenzelHawking <denzelhawking@gmail.com>
2020-10-23 19:46:53 +0530

    out projects tab added

--- js/request-form.js
+++ js/request-form.js
@@ -1,3 +1,15 @@
-contactForm.onsubmit = (e) => {
+const form = document.getElementById('contact-form');
+
+form.onsubmit = (e) => {
   e.preventDefault();
+  // submitForm('../server/sendForm.pnp', 'POST', new FormData(form));
+}
+
+
+function submitForm (url, method, data) {
+  fetch(url, {
+    method: method,
+    body: data
+  })
+  .then()
 }
