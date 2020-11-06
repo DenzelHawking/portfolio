df9ea5e9af79134a104fd57c5291b89e92e4ff99
Kirill Anikin <anikinkirill22@gmail.com>
2020-11-06 22:58:11 +0500

    feat: loader added

--- src/index.js
+++ src/index.js
@@ -3,3 +3,8 @@ import "./css/index.css";
 import "./js/toggle-tab.js";
 import "./js/particles/particles.js";
 import "./js/particles/particles-config.js";
+
+// TODO: need to be replaced
+window.addEventListener("load", () => {
+  setTimeout(() => document.body.classList.add("loaded"), 500);
+});
